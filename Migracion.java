99fea9654a6d6ed8e5c3b31c61289ec22dda636a
Rafael Ramírez <mrsblast@gmail.com>
2016-10-12 03:53:39 -0400

    Clases Migracion y Reporte Templates

---
+++ Migracion.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+public class Migracion{
+    private ArrayList<Ciudad> listaCiudades;
+    private Grafo mapa;
+    private Grafo flujoEmigrantes;
+
+    public Migracion(){
+        this.listaCiudades = new ArrayList<Ciudad>();
+        this.mapa = new Grafo();
+        this.flujoEmigrantes = new Grafo();
+    }
+
+    public ArrayList<Reporte> flujoEmigrantes(String nombreCiudad){
+    
+    
+    }
+
+    public void mostrarFlujoEmigrantes(String nombreCiudad){
+    
+    
+    }
+
+    public String fechaMigracion(String nombre, String apellido){
+    
+    
+    }
+
+    public ArrayList<PersonaSaliente> listaGeneral(){
+    
+    
+    }
+
+    public Grafo representacionMatrizDePesos(ArrayList<PersonaSaliente> listaEmigrantes){
+    
+    
+    }
+
+    public void mostrarMatrizDePesos(){
+    
+    
+    }
+
+    public void menu(){
+    
+    
+    }
+
+    public static void main(String args[]){
+    
+    
+    }
+
+
+}

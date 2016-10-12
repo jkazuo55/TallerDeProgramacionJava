23f8f7d65296d5a8475cb9eeeea5be0c16509250
jkazuo55 <jkazuo555@gmail.com>
2016-10-12 03:58:10 -0400

    MejorandoClasePersonaEmigrante

--- PersonaEmigrante.java
+++ PersonaEmigrante.java
@@ -2,21 +2,24 @@ public class PersonaEmigrante extends Persona{
 	public String destino;
 	public String fecha;
 
-	public PersonaEmigrante(String nombre, String cedula , String nacidoEn, String sexo, int edad,String destino, String fecha){
-		super(nombre, cedula ,nacidoEn,sexo,edad);
+	public PersonaEmigrante(String nombre,String apellido, String sexo , String cedula,String nacidoEn, int edad,String destino, String fecha){
+		super(nombre,apellido,sexo,cedula,nacidoEn,edad);
 		this.destino=destino;
 		this.fecha=fecha;
 	}
+
+	//========Metodos Setters========
 	public void setDestino( String destino){
 		this.destino=destino;
 	}	
+	public void setFecha(String fecha){
+		this.fecha=fecha;
+	}
+	//=========Metosos Getters==========
 	public String getDestino(){
 		return this.destino;
 	}
 
-	public void setFecha(String fecha){
-		this.fecha=fecha;
-	}
 	public String getFecha(){
 		return this.fecha;
 	}
